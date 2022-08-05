c91e98e3dada6815af2c3004f2e3a23b02a83bd7
violanerd <madalyne.cross@gmail.com>
2022-08-05 14:54:14 -0500

    added fetch call to script js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 const $animalForm = document.querySelector('#animal-form');
 
 const handleAnimalFormSubmit = event => {
@@ -26,6 +28,26 @@ const handleAnimalFormSubmit = event => {
   }
   const animalObject = { name, species, diet, personalityTraits };
 
-};
+  fetch('api/animals', {
+    method: 'POST',
+    headers: {
+      Accept: 'application.json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
+  })
+    .then(response =>{
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
+
+}
+
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
